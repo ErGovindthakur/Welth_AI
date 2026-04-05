c9e4a7dccb2ed7ffa14aaf5141f10b4bb47bf458
ErGovindthakur <ergovindthakur@gmail.com>
2026-04-06 04:16:55 +0530

    fix clerk middleware matcher

--- middleware.js
+++ middleware.js
@@ -54,8 +54,35 @@
 
 
 // My updated part without arcjet
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
+
+// const isProtectedRoute = createRouteMatcher([
+//   "/dashboard(.*)",
+//   "/account(.*)",
+//   "/transaction(.*)",
+// ]);
+
+// export default clerkMiddleware(async (auth, req) => {
+//   const { userId } = await auth();
+
+//   if (!userId && isProtectedRoute(req)) {
+//     const { redirectToSignIn } = await auth();
+//     return redirectToSignIn();
+//   }
+
+//   return NextResponse.next();
+// });
+
+// export const config = {
+//   matcher: [
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|png|gif|svg|ico)).*)",
+//     "/(api|trpc)(.*)",
+//   ],
+// };
+
+// Third updated code 
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
@@ -64,19 +91,13 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
+  const { userId, redirectToSignIn } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
-    const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
-
-  return NextResponse.next();
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|png|gif|svg|ico)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next).*)"],
 };
